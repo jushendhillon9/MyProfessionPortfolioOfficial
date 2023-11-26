821890d12846db3e6568ba50fc6fdc107b90a4fe
jushen dhillon <jushendhillon@London-Macbook.local>
2023-11-26 15:45:10 -0800

    troubleshooting port in heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const path = require('path');
-const port = 3001;
+const port = process.env.PORT || 3001;
 const routes = require("./controllers");
 
 app.use(express.json());
